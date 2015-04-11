fd1a8655c1cd22e5d644459d540d99216b1bcb8e
Paul Hunt <paul.c.hunt@gmail.com>
2015-04-11 14:23:46 +0100

    Added sign up form

--- mezzcaptcha/forms.py
+++ mezzcaptcha/forms.py
@@ -1,8 +1,16 @@
-from mezzanine.generic.forms import ThreadedCommentForm                                                      
+from mezzanine.generic.forms import ThreadedCommentForm
+from mezzanine.accounts.forms import ProfileForm
 from captcha.fields import CaptchaField
 
-class GuestCommentForm(ThreadedCommentForm):                                                           
-    def __init__(self, request, *args, **kwargs):                                                            
-        super(GuestCommentForm, self).__init__(request, *args, **kwargs)                               
-        if not request.user.is_authenticated():                                                              
+
+class GuestCommentForm(ThreadedCommentForm):
+    def __init__(self, request, *args, **kwargs):
+        super(GuestCommentForm, self).__init__(request, *args, **kwargs)
+        if not request.user.is_authenticated():
             self.fields['captcha'] = CaptchaField()
+
+
+class SignUpCaptchaForm(ProfileForm):
+    def __init__(self, request, *args, **kwargs):
+        super(SignUpCaptchaForm, self).__init__(request, *args, **kwargs)
+        self.fields['captcha'] = CaptchaField()
